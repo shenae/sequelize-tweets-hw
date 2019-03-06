179f277eadef6b97ebb1b66446e7fb4fc5c6ea66
jkarlin929 <osida11572@gmail.com>
2019-03-06 14:28:00 -0500

    edit id name in data file

--- data.js
+++ data.js
@@ -1,36 +1,36 @@
 const data = [
   {
-    "id": 1,
+    "tweet_id": 1,
     "text": "I'm releasing new music this month",
     "hashtag": "#new",
     "retweets": 72,
   },
   {
-    "id": 2,
+    "tweet_id": 2,
     "text": "Should I do a cooking show?",
     "hashtag": "#cooking",
     "retweets": 10
   },
   {
-    "id": 3,
+    "tweet_id": 3,
     "text": "Would love to see some negative responses too. If Hooks aren’t working out for you, share your experiences!",
     "hashtag": "#react",
     "retweets": 600
   },
   {
-    "id": 4,
+    "tweet_id": 4,
     "text": "In Sweden, you can buy toilet paper called Kräpp.",
     "hashtag": "#sweden",
     "retweets": 413
   },
   {
-    "id": 5,
+    "tweet_id": 5,
     "text": "In 1973, the entire Internet consisted of only 42 computers.",
     "hashtag": "#wow",
     "retweets": 89
   },
   {
-    "id": 6,
+    "tweet_id": 6,
     "text": "Brad Pitt's first employer was El Pollo Loco (Crazy Chicken) where he had to dress as a chicken, hand out flyers, and attract customers to the restaurant.",
     "hashtag": "#glowup",
     "retweets": 55
---
+++ seed.js
@@ -0,0 +1,8 @@
+//import your models on this line
+
+const main = async () => {
+  // 1. Delete everything in the database
+
+};
+
+main();
