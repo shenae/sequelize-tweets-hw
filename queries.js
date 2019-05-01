218183b5dfedb023a91a490f14ae873561b78e44
Shenae Simmons <shhnae@gmail.com>
2019-05-01 14:46:49 -0400

    completed the homework

--- queries.js
+++ queries.js
@@ -1,17 +1,59 @@
 //import your models on this line
-
-//require sequlize and the Operator value. Check the docs, if you
+const { tweets_table } = require('./models');
+//require sequelize and the Operator value. Check the docs, if you
 //want to check the syntax for that.
+const { Op } = require('sequelize');
 
+const main = async () => {
+    try {
 //1. Write a sequelize query to find all the tweets.
-
+const allDaTweets = await tweets_table.findAll();
+//console.log(allDaTweets);
 //2. Write a query to find the tweet with a hashtag: "#react"
+const reactTweets = await tweets_table.findAll( {
+        	where: {
+			hashtag: '#react'
+			}
+        });
+//console.log(reactTweets);
+	
 
 //3. Write a query to find the tweet with more than 50 retweets.
 
+const moreRetweets = await tweets_table.findAll({
+		where: {
+			retweets: 
+			{
+				[Op.gt]: 50
+			}
+		}
+	});
+    //console.log(moreRetweets);
 //4. Write a query to find the tweet with an id of 3
-
+const tweetId = await tweets_table.findAll( {
+    where: {
+    id: 3
+    }
+});
+//console.log(tweetId);
 //5. Write a query to delete the tweet with an id of 3
+const deleteTweet = await tweets_table.destroy( {
+    where: {
+    id: 3
+    }
+});
+
+
+ } catch (error) {
+    console.log('Something is not right, Shenae: ' + error);
+}
+// const run = async () => {
+//     await main();
+//     process.exit();
+//   }
+}  
+  main();
+  
 
 //BONUS SECTION
 //6. Write a query to create a new tweet with any data you want
--- seed.js
+++ seed.js
@@ -1,20 +1,35 @@
 //import your models on this line
-const { Tweets } = require('./models');
+const { tweets_table } = require('./models');
+
 
 const main = async () => {
-
+  try {
   // 1. Using your models, create new tweets with
   //your the data found in `data.js`. Be sure to have at least one tweet with
   //retweets with an integer greater than 50 (you'll see why)
 
   //UNCOMMENT THE EXAMPLE TO TEST THE SEED FILE!
   //EXAMPLE:
-//   const firstTweet = await Tweets.create({
-//     text: 'Hello, is it me you're looking for?,
-//     hashtag: '#lionel',
-//     retweets: 42
-//   });
-//   console.log('this is firstTweet', firstTweet.dataValues);
-// };
+
+  const firstTweet = await tweets_table.create({
+    text: 'Hello is it me youre looking for',
+    hashtag: '#lionel',
+    retweets: 42
+  });
+  console.log('this is firstTweet', firstTweet.dataValues);
+  const secondTweet = await tweets_table.create({
+    text: 'I am about to neutralize this sequelize',
+    hashtag: '#programmerhumour',
+    retweets: 55
+  });
+  console.log('this is secondTweet', secondTweet.dataValues);
+
+} catch (error) {
+  console.log('Something went wrong: ' + error);
+} finally {
+  process.exit();
+}
+
+}
 
 main();
